551217a7d9e5d1d322b10d01346d7c8aa4d4c2fd
Nicholas Nam <62817307+n1ckDotEXE@users.noreply.github.com>
2020-04-28 14:08:58 -0400

    Create Scene It Part 1

--- AJAX 102/script.js
+++ AJAX 102/script.js
@@ -1,13 +1,19 @@
-const dogButton = document.getElementById('dog-button');
-dogButton.addEventListener('click', function () {
-  dogButton.textContent = 'Generating Doggo...';
-  $.get('https://dog.ceo/api/breeds/image/random').then(function (data) {
-    const img = document.createElement('img');
-    console.log('data = ', data);
-    img.setAttribute('src', data.message);
-    const imagesDiv = document.getElementById('images');
-    imagesDiv.innerHTML = '';
-    imagesDiv.appendChild(img);
-    dogButton.textContent = 'Generate Doggo';
+window.addEventListener('DOMContentLoaded', function() {
+
+$.get("https://dog.ceo/api/breeds/list")
+  .then(function(data) {
+    const breeds = data.message;
+
+    breeds.forEach(function(breed) {
+      const option = document.createElement('option');
+      option.setAttribute('value', breed) {
+        option.textContent = breed;
+        select.appendChild(option);
+        // const select = document.getElementById('dog-select'); <-- Another Alternative
+      };
+
+
+    });
   });
+
 });
